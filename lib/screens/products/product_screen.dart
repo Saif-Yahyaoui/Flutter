1541e441e3e2e8cf34b40ce2bbaa0235b0e49548
rania hachem <rania.hachem@esprit.tn>
2023-12-05 00:11:13 +0100

    affichage par catégorie produit

--- lib/screens/products/product_screen.dart
+++ lib/screens/products/product_screen.dart
@@ -16,7 +16,7 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-  final List<Product> products = [
+  List<Product> products = [
     Product(
       title: 'Korean BBQ Burger',
       price: 8,
@@ -48,6 +48,8 @@ class _ProductListState extends State<ProductList> {
       category: 'Healthy',
     ),
   ];
+
+  String selectedCategory = 'All';
 
   Future<void> confirmDeleteProduct(Product product) async {
     return showDialog<void>(
@@ -82,21 +84,55 @@ class _ProductListState extends State<ProductList> {
     });
   }
 
+  List<String> getCategories() {
+    Set<String> categories = {'All'};
+    for (var product in products) {
+      categories.add(product.category);
+    }
+    return categories.toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // Trois cartes par ligne
-        crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
-      ),
-      itemCount: products.length,
-      itemBuilder: (context, index) {
-        return ProductCard(
-          product: products[index],
-          onDelete: () => confirmDeleteProduct(products[index]),
-        );
-      },
+    List<String> categories = getCategories();
+
+    return Column(
+      children: [
+        DropdownButton<String>(
+          value: selectedCategory,
+          onChanged: (String? value) {
+            setState(() {
+              selectedCategory = value!;
+            });
+          },
+          items: categories.map((String category) {
+            return DropdownMenuItem<String>(
+              value: category,
+              child: Text(category),
+            );
+          }).toList(),
+        ),
+        Expanded(
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              if (selectedCategory == 'All' || products[index].category == selectedCategory) {
+                return ProductCard(
+                  product: products[index],
+                  onDelete: () => confirmDeleteProduct(products[index]),
+                );
+              } else {
+                return SizedBox.shrink(); // Retourne un widget vide pour les produits qui ne correspondent pas à la catégorie sélectionnée
+              }
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -111,7 +147,7 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 4.0,
-      color: Color.fromARGB(255, 193, 255, 211), // Changer la couleur de fond
+      color: Color.fromARGB(255, 193, 255, 211),
       child: Container(
         padding: const EdgeInsets.all(8.0),
         child: Column(
