1830d812f2358b2eb0fd008bd36add525f3e2c1c
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2023-12-11 02:02:11 +0100

    Create the Donation SideMenu

---
+++ lib/models/Donation.dart
@@ -0,0 +1,17 @@
+class Donation {
+  String title;
+  String description;
+  DateTime date;
+  int quantity;
+  String status;
+  String image;
+
+  Donation({
+    required this.title,
+    required this.description,
+    required this.date,
+    required this.quantity,
+    required this.status,
+    required this.image,
+  });
+}
