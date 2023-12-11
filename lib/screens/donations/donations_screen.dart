7e5d43aade738e8c6ee5ec2acaa6b32fac652b8b
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2023-12-11 23:27:23 +0100

    Web Service Consummation

--- lib/screens/donations/donations_screen.dart
+++ lib/screens/donations/donations_screen.dart
@@ -1,51 +1,72 @@
-import 'package:admin/responsive.dart';
-import 'package:admin/screens/dashboard/components/my_fields.dart';
+import 'package:admin/screens/donations/add_donations_screen.dart';
 import 'package:flutter/material.dart';
+import '../../service/DonationApiService.dart';
 
-import '../../constants.dart';
-import '../dashboard/components/header.dart';
-import '../dashboard/components/recent_files.dart';
-import '../dashboard/components/storage_details.dart';
+class DonationScreen extends StatefulWidget {
+  @override
+  _DonationScreenState createState() => _DonationScreenState();
+}
 
+class _DonationScreenState extends State<DonationScreen> {
+  final DonationApiService apiService = DonationApiService(baseUrl: 'http://localhost:7001');
+  late Future<List<Map<String, dynamic>>> donations;
 
-class DonationsScreen extends StatelessWidget {
+  @override
+  void initState() {
+    super.initState();
+    donations = apiService.getAllDonations();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        primary: false,
-        padding: EdgeInsets.all(defaultPadding),
-        child: Column(
-          children: [
-            Header(),
-            SizedBox(height: defaultPadding),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Column(
-                    children: [
-                      MyFiles(),
-                      SizedBox(height: defaultPadding),
-                      RecentFiles(),
-                      if (Responsive.isMobile(context))
-                        SizedBox(height: defaultPadding),
-                      if (Responsive.isMobile(context)) StorageDetails(),
-                    ],
-                  ),
-                ),
-                if (!Responsive.isMobile(context))
-                  SizedBox(width: defaultPadding),
-                // On Mobile means if the screen is less than 850 we don't want to show it
-                if (!Responsive.isMobile(context))
-                  Expanded(
-                    flex: 2,
-                    child: StorageDetails(),
-                  ),
-              ],
-            )
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Donations'),
+          actions: [
+            // Button in the app bar on the right
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => POST()),
+                  );                },
+                child: Text('Add Donations'),
+              ),
+            ),
           ],
+        ),
+        body: FutureBuilder<List<Map<String, dynamic>>>(
+          future: donations,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Text('No donations available.');
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Quantity: ${snapshot.data![index]['quantite']}'),
+                        Text('Date: ${snapshot.data![index]['date']}'),
+                        Text('Status: ${snapshot.data![index]['etat']}'),
+                        // Add more fields as needed
+                      ],
+                    ),
+                    // Add more features to the ListTile if required
+                  );
+                },
+              );
+            }
+          },
         ),
       ),
     );
