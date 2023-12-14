9896ae2d1967bb90d1d0f91298fe7d4a65524941
Saif Yahyaoui <saif.yahyaoui@esprit.tn>
2023-12-14 11:41:06 +0100

    Merge with GestionUser

--- lib/screens/Signup/components/signup_form.dart
+++ lib/screens/Signup/components/signup_form.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
+import 'package:http/http.dart' as http;
+
 
 class SignUpForm extends StatefulWidget {
-  const SignUpForm({
+
+  SignUpForm({
     Key? key,
   }) : super(key: key);
 
@@ -14,21 +19,73 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  TextEditingController username = TextEditingController();
+
+  TextEditingController password = TextEditingController();
+
+  TextEditingController email = TextEditingController();
+
+  TextEditingController phonenumber = TextEditingController();
+
+  TextEditingController adresse = TextEditingController();
+
+  TextEditingController role = TextEditingController();
+
+  bool _isNotValidate = false;
+
+  void registerUser() async{
+    if(username.text.isNotEmpty && password.text.isNotEmpty && email.text.isNotEmpty && phonenumber.text.isNotEmpty && adresse.text.isNotEmpty && role.text.isNotEmpty ){
+      var registerBody = {
+        "username" :username.text,
+        "password" :password.text,
+        "email" :email.text,
+        "phoneNumber" :phonenumber.text,
+        "adresse" :adresse.text,
+        "role" :role.text,
+      };
+      var response= await http.post(Uri.parse('http://localhost:7001/user/users'),
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode(registerBody)
+      );
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse['status']);
+      if(jsonResponse['status']){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const LoginScreen();
+            },
+          ),
+        );
+      }else{
+        print("something went wrong");
+      }
+    }else{
+      setState(() {
+        _isNotValidate=true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
           TextFormField(
-            keyboardType: TextInputType.emailAddress,
+            controller: username,
+            keyboardType: TextInputType.name,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
-            onSaved: (email) {},
-            decoration: const InputDecoration(
+
+            decoration:  InputDecoration(
               hintText: "username",
+              errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+              errorStyle: TextStyle(color: Colors.red),
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.person_2),
+                child: Icon(Icons.person),
               ),
             ),
           ),
@@ -36,10 +93,13 @@ class _SignUpFormState extends State<SignUpForm> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defaultPadding),
             child: TextFormField(
+              controller: password,
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: kPrimaryColor,
-              decoration: const InputDecoration(
+              decoration:  InputDecoration(
+                errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+                errorStyle: TextStyle(color: Colors.red),
                 hintText: "Your password",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(defaultPadding),
@@ -48,12 +108,16 @@ class _SignUpFormState extends State<SignUpForm> {
               ),
             ),
           ),
+          SizedBox(height: 10),
           TextFormField(
+            controller: email,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
             onSaved: (email) {},
-            decoration: const InputDecoration(
+            decoration:  InputDecoration(
+              errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+              errorStyle: TextStyle(color: Colors.red),
               hintText: "email",
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
@@ -61,49 +125,63 @@ class _SignUpFormState extends State<SignUpForm> {
               ),
             ),
           ),
+          SizedBox(height: 25),
           TextFormField(
-            keyboardType: TextInputType.emailAddress,
+            controller: phonenumber,
+            keyboardType: TextInputType.phone,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
             onSaved: (email) {},
-            decoration: const InputDecoration(
+            decoration:  InputDecoration(
+              errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+              errorStyle: TextStyle(color: Colors.red),
               hintText: "phoneNumber",
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.email),
+                child: Icon(Icons.phone),
               ),
             ),
           ),
+          SizedBox(height: 25),
           TextFormField(
+            controller: adresse,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
             onSaved: (email) {},
-            decoration: const InputDecoration(
+            decoration:  InputDecoration(
               hintText: "adresse",
+              errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+              errorStyle: TextStyle(color: Colors.red),
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.email),
+                child: Icon(Icons.place),
               ),
             ),
           ),
+          SizedBox(height: 25),
           TextFormField(
+            controller: role,
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
             onSaved: (email) {},
-            decoration: const InputDecoration(
+            decoration:  InputDecoration(
               hintText: "role",
+              errorText: _isNotValidate ? "Enter correct inforamtions" :null,
+              errorStyle: TextStyle(color: Colors.red),
               prefixIcon: Padding(
                 padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.email),
+                child: Icon(Icons.person_search),
               ),
             ),
           ),
-
+          SizedBox(height: 10),
           const SizedBox(height: defaultPadding / 2),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: ()=> {
+              registerUser()
+            },
             child: Text("Sign Up".toUpperCase()),
           ),
           const SizedBox(height: defaultPadding),
